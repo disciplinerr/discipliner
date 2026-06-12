c39c08ddd695f9f7b8368a15964c73e67f4bde3d
FernandoHaeser <fernandohaeserr@gmail.com>
2026-06-11 21:48:39 -0300

    chore: update webapp title with lowerCase

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Discipliner",
+  title: "discipliner",
   description: "Discipline and study tracker. No shortcuts.",
 };
 
